17b7b70ed83eb7ce5cfec546e5a1fbb287bd509e
Robel Isaias <robelisaias@gmail.com>
2026-07-20 21:09:48 -0400

    initial structure

---
+++ src/app/features/events/event-filters/event-filters.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-filters',
+  imports: [],
+  templateUrl: './event-filters.html',
+  styleUrl: './event-filters.css',
+})
+export class EventFilters {}
